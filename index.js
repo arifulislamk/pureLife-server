9158e05cf743cf50b27623c98fa1723076e54b10
arifulislamk <ariful18374@gmail.com>
2024-06-03 11:50:32 +0600

    cheek

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
+console.log(process.env.DB_PASS)
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
